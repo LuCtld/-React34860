7f0f0b65afed63ef8719ab66a3f80901738242bf
Lucia cataldo <lcataldo.lc@gmail.com>
2022-12-20 19:05:33 -0300

    Clase3

---
+++ src/componentsNavbar/Navbar.js
@@ -0,0 +1,16 @@
+//Navbar
+
+const Navbar = () => {
+    return (
+        <nav>
+        <h1>Ecommerce</h1>
+        <div>
+            <button>Pitas</button>
+            <button>Parafinas</button>
+            <button>Quillas</button>
+        </div>
+        </nav>
+    )
+}
+
+export default Navbar
