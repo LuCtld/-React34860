1549e42b56e3630edbdaea82ef0d9ce1d2fd117a
Lucia cataldo <lcataldo.lc@gmail.com>
2023-02-06 21:02:53 -0300

    catalogo

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,37 +2,36 @@ import './ItemListContainer.css'
 import { useState, useEffect} from 'react'
 import ItemList from '../ItemList/ItemList.js'
 import { useParams } from 'react-router-dom'
-import Loader from '../Loader/Loader'
-import { getDocs, collection, query, where } from 'firebase/firestore'
+import { getDocs, collection } from 'firebase/firestore'
 import { db } from '../../services/firebase/firebaseConfig'
 
 
 const ItemListContainer = ({greeting}) => {
     const [products, setProducts]= useState([])
-    const [Loading, setLoading]= useState()
 
     const { categoryId } = useParams()
 
     useEffect(() => {
-
-
         (async() => {
 
-            const productsRef = categoryId
-            ? query (collection (db, 'products'), where ('category', '==', categoryId))
-            : collection(db, 'products')
 
-            getDocs(productsRef).then(response => {
-                const productsAdapted = response.docChanges.map(doc =>{
-                    const data = doc.data()
-                    return {id: doc.id, ...data}
+            const productsRef = collection(db, 'products')
+
+            try{
+                const snapshot = await getDocs(productsRef)
+                const productsAdapted = snapshot.docs.map(doc => {
+                    return {id: doc.id, ...doc.data()}
                 })
-                setProducts (productsAdapted)
-            })
+                setProducts(productsAdapted)
+
+            } catch(error){
+                console.log(error)
+            }
 
     })()
-},[])
+    },[])
 
+console.log(products, 'productos')
     return (
         <div >
             <h1 className="titulos-principales">{greeting}</h1>
