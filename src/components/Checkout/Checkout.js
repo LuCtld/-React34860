79bfcaf90ebf3a7906fcf89278d561dbfe84e493
Lucia cataldo <lcataldo.lc@gmail.com>
2023-02-09 20:37:25 -0300

    Clear cart

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -6,55 +6,88 @@ import "./Checkout.css"
 
 
 const Checkout = () => {
-
-    const {cart, total, clear} = useContext(CartContext)
-    const [name, setName] = useState("")
-    const [phone,setPhone] = useState("")
-    const [email, setEmail] = useState("")
-
+    const { cart, total, clear } = useContext(CartContext);
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+    const [showForm, setShowForm] = useState(true);
+    const [orderId, setOrderId] = useState(null);
 
     const createOrder = async () => {
-        const objOrder = {
+    const objOrder = {
         name: name,
         phone: phone,
         email: email,
         items: cart,
-        total
-        }
-        const col = collection(db,"Orders")
-        const order = await addDoc(col,objOrder)
-        console.log("El ID es:",order.id)
-        clear()
+        total,
+    };
+    const col = collection(db, "Orders");
+    const order = await addDoc(col, objOrder);
+    setOrderId(order.id);
+    setShowForm(false);
+    clear();
     }
+    
 
 
-    return (
-        <div>
-            <h2 className="titulo-checkout">Checkout</h2>
 
+
+
+
+
+
+
+
+
+
+
+
+return (
+    <div>
+        <h2 className="titulo-checkout">Checkout</h2>
+
+
+        {showForm ? (
+        <form className="formulario">
             <div>
-            <h2 className="titulo-checkout-dos">Ingrese sus datos</h2>
-            </div>
-
-            <form className="formulario">
             <div>
-                <input type="text" value={name} placeholder="Nombre"  onChange={(event)=> setName(event.target.value)}/>
-            </div>
-
-            <div>
-                <input type="text" value={phone} placeholder="Telefono"  onChange={(event)=> setPhone(event.target.value)}/>
-            </div>
-
-            <div>
-                <input type="text" value={email} placeholder="Mail"  onChange={(event)=> setEmail(event.target.value)}/>
-            </div>
-
-            </form>
-
-            <button className="boton-checkout"onClick={createOrder} >Generar Orden</button>
-
+        <h2 className="titulo-checkout-dos">Ingrese sus datos</h2>
         </div>
-    )
-}
+            <input
+                type="text"
+                value={name}
+                placeholder="Nombre"
+                onChange={(event) => setName(event.target.value)}
+            />
+        </div>
+
+        <div>
+            <input
+                type="text"
+                value={phone}
+                placeholder="Telefono"
+                onChange={(event) => setPhone(event.target.value)}
+            />
+            </div>
+
+            <div>
+            <input
+                type="text"
+                value={email}
+                placeholder="Mail"
+                onChange={(event) => setEmail(event.target.value)}
+            />
+            </div>
+        </form>
+    ) : (
+        <div className="textoid">Su ID de compra es: {orderId}</div>
+    )}
+
+        <button className="boton-checkout" onClick={createOrder}>
+        Generar Orden
+        </button>
+    </div>
+);
+};
 
 export default Checkout;
