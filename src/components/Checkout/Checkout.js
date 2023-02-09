26a49deffd96ede3d3351e8c43401243ba984776
Lucia cataldo <lcataldo.lc@gmail.com>
2023-02-09 20:53:37 -0300

    error

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -14,6 +14,7 @@ const Checkout = () => {
     const [orderId, setOrderId] = useState(null);
 
     const createOrder = async () => {
+        setLoading(true)
     const objOrder = {
         name: name,
         phone: phone,
@@ -23,23 +24,60 @@ const Checkout = () => {
     };
     const col = collection(db, "Orders");
     const order = await addDoc(col, objOrder);
-    setOrderId(order.id);
-    setShowForm(false);
-    clear();
+
+
+
+    const batch = writeBatch(db)
+
+    const ids = cart.map(prod => prod.id)
+    console.log(ids)
+
+    const productsRef = query(collection(db, 'products'), where(documentId(), 'in', ids))
+
+
+    const productsAddedToCartFromFirestore = await getDocs(productsRef)
+    const { docs } = productsAddedToCartFromFirestore
+
+    const outOfStock = []
+
+    docs.forEach(doc => {
+        const dataDoc = doc.data()
+        const stockDb = dataDoc.stock
+
+        const productAddedToCart = cart.find(prod => prod.id === doc.id)
+        const prodQuantity = productAddedToCart.quantity
+
+        if(stockDb >= prodQuantity) {
+            batch.update(doc.ref, { stock: stockDb - prodQuantity})
+        } else {
+            outOfStock.push({ id: doc.id, ...dataDoc})
+        }
+    })
+
+    if(outOfStock.length === 0) {
+        await batch.commit()
+
+        const orderRef = collection(db, 'orders')
+
+        const orderAdded = await addDoc (orderRef, objOrder)
+
+        const { id } = orderAdded
+        setOrderId(order.id)
+
+        clear()
+
+
+        console.log(id)
+    } else {
+        console.error('hay productos fuera de stock')
     }
-    
+} catch (error) {
+    console.error(error)
+} finally {
+    setLoading(false)
+}
 
-
-
-
-
-
-
-
-
-
-
-
+setShowForm(false);
 
 
 return (
@@ -88,6 +126,6 @@ return (
         </button>
     </div>
 );
-};
+}
 
 export default Checkout;
